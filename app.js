43ced5cd9115616f9da29d951e9b978573941d5e
Jeremy Rice <jdavidrice@gmail.com>
2021-01-19 22:35:11 -0500

    Remove const util from app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
